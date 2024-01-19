dd58c147c3bda56f6a21d7eb159ebb72d688ca66
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2024-01-19 18:16:39 +0200

    add pagination views error

--- views/task/task.php
+++ views/task/task.php
@@ -1,5 +1,9 @@
 <?php
 
+use yii\helpers\Html;
+use yii\widgets\LinkPager;
+
+
 /** @var yii\web\View $this */
 
 $this->title = 'Статистика відпрацьованих заявок';
@@ -11,41 +15,76 @@ $this->title = 'Статистика відпрацьованих заявок';
         <form method="post" action="/?r=task/index">
             <input type="hidden" name="<?= Yii::$app->request->csrfParam ?>" value="<?= Yii::$app->request->getCsrfToken() ?>"/>
             <p class="fs-3">Статистика відпрацьованих заявок за період</p>
-            <input type="date" name="startdate" value="<?= date("Y-m-");?>01" />
+            <input type="date" name="startdate" value="<?= $startDate ?? date("Y-m-").'01'?>"/>
             -
-            <input type="date" name="enddate" value="<?= date("Y-m-d");?>" />
+            <input type="date" name="enddate" value="<?= $endDate ?? date("Y-m-d")?>"/>
             &nbsp;
             <input class="btn btn-primary" type="submit" value="Отримати">
         </form>
 
     </div>
 
-                       <table class="category-table">
-                            <thead>
-                            <tr>
-                                <th>ФІО / Хто вирішив проблему</th>
-                                <th>Відключення</th>
-                                <th>Перевірка / сдешевлення</th>
-                                <th>Технічне питання</th>
-                                <th>Інше</th>
-                                <th>Усього</th>
-                            </tr>
-                            </thead>
-                            <tbody>
+    <?php if ($errorMsg): ?>
 
-                            <?php foreach ($rows as $row): ?>
-                                <tr>
-                                    <td><?= $row['user_name'] ?></td>
-                                    <td><?= $row['category1_count'] ?></td>
-                                    <td><?= $row['category2_count'] ?></td>
-                                    <td><?= $row['category3_count'] ?></td>
-                                    <td><?= $row['category4_count'] ?></td>
-                                    <td><?= $row['row_summ'] ?></td>
-                                </tr>
-                            <?php endforeach; ?>
+        <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+            <div class="alert alert-primary" role="alert">
+                <?= $errorMsg ?>
+            </div>
+        </div>
 
-                            </tbody>
-                        </table>
+    <?php else: ?>
+
+        <?php if (count($rows) > 0): ?>
+
+            <table class="category-table">
+                <thead>
+                <tr>
+                    <th>ФІО / Хто вирішив проблему</th>
+                    <th>Відключення</th>
+                    <th>Перевірка / сдешевлення</th>
+                    <th>Технічне питання</th>
+                    <th>Інше</th>
+                    <th>Усього</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php foreach ($rows as $row): ?>
+                    <tr>
+                        <td><?= $row['user_name'] ?></td>
+                        <td><?= $row['category1_count'] ?></td>
+                        <td><?= $row['category2_count'] ?></td>
+                        <td><?= $row['category3_count'] ?></td>
+                        <td><?= $row['category4_count'] ?></td>
+                        <td><?= $row['row_summ'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+
+                </tbody>
+            </table>
+
+            <nav aria-label="Page navigation">
+                <?= LinkPager::widget(['pagination' => $pagination]) ?>
+                <p>for example 5 notes per page</p>
+            </nav>
+
+        <?php else: ?>
+
+            <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+                <div class="alert alert-success" role="alert">
+                    нічого не знайдено
+                </div>
+            </div>
+
+        <?php endif; ?>
+
+    <?php endif; ?>
+
+
+
+</div>
+
+
 
 
 
